7d1658502f8760db6f5eef81282117cafceee9c3
sarge <sarge.lutta@gmail.com>
2023-02-24 10:15:27 +0300

    Changes to be committed: 	modified:   blog/static/main.css 	modified:   blog/templates/base.html 	deleted:    django_project/.env 	modified:   django_project/__pycache__/settings.cpython-39.pyc 	modified:   django_project/__pycache__/urls.cpython-39.pyc 	modified:   django_project/settings.py	modified:   django_project/urls.py 	modified:   users/__pycache__/apps.cpython-39.pyc 	modified:   users/__pycache__/views.cpython-39.pyc 	modified:   users/templates/login.html

--- django_project/settings.py
+++ django_project/settings.py
@@ -135,9 +135,3 @@ LOGIN_REDIRECT_URL = 'blog-home'
 
 LOGIN_URL = 'login'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'sargeelvis@gmail.com'
-EMAIL_HOST_PASSWORD = 'rxcgmuzrxtzlokpn'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
